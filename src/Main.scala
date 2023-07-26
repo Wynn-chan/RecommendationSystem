8aeec8842f14a65741faa23d38c2ad05d1bb58bb
Erica Bertuzzi <37706408+Wynn-chan@users.noreply.github.com>
2023-07-26 10:55:45 +0200

    spostamento dile in src

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,59 @@
+import scala.io.Source
+import scala.io.StdIn.readLine
+
 object Main {
+
+  val inputFile = "src/prova.txt"
+  val exc = "src/ignore.txt"
+  val path = "src/books.csv"
+
+  // wordcount in Scala
+  def wordCountScala(s: String) = {
+
+    val exclude = Source.fromFile(exc).getLines().toList.flatMap(line => line.split((" ")))
+   // println(exclude)
+    //var allWords = Source.fromFile(fileName).getLines.toList.flatMap(line => line.split(" ")).map(w => (w.filter(_.isLetter).toLowerCase))
+    var s1 = s.replace("-", " ")
+    s1 = s1.replace("\"", "")
+    s1 = s1.replace("\'", "")
+    s1 = s1.replace("  ", " ")
+    s1 = s1.replace("  ", " ").toLowerCase()
+    //var s1 = s
+    //s1 = s1.map(w => (if (!(w.isLetter || w.isSpaceChar)) w = ' ' ))
+    var allWords = s1.split(" ")
+    allWords = allWords.filterNot(element => exclude.contains(element))
+    println(s)
+    val emptyMap = Map[String, Int]() withDefaultValue 0
+    allWords.foldLeft(emptyMap)((a, w) => (a) + (w -> (a(w) + 1)))
+  }
+
+  def printCSV(list: List[(String, String, String, String)], i: Int): Unit = {
+    if (i != 0) {
+      def s = list.head
+      println("Title: " + s._1 + "\nSubtitle: " + s._2 + "\nTag: " + s._3 + " \nDescription:" + s._4)
+      printCSV(list.tail, i-1)
+    }
+
+  }
+
   def main(args: Array[String]): Unit = {
+
+    val pcsv = Source.fromFile(path).getLines().toList
+    val pcsvtxt = pcsv.map(x => x.split(",")).map(x => (x(2), x(3), x(5), x(7)));
+    println(pcsvtxt.head)
+    println(pcsvtxt(5))
+
+    //pcsvtxt.foreach(s => println("Titolo: " + s._1 + "\nSottotitolo: " + s._2 + "\nTag: " + s._3 + " \nDescription:" + s._4))
+
+    //printCSV(pcsvtxt.tail, 4)
+    //println(wordCountScala(pcsvtxt(1)._1 + " " + pcsvtxt(1)._2 + " " + pcsvtxt(1)._3 + " " + pcsvtxt(1)._4))
+    (1 to 4).foreach(n => println(wordCountScala(pcsvtxt(n)._1 + " " + pcsvtxt(n)._2 + " " + pcsvtxt(n)._3 + " " + pcsvtxt(n)._4))
+      )
+
+
     println("Hello world!")
   }
+
+
+
 }
